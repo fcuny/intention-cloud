0da820ac5b3f0f3e63cdc109bc567fe2a76ead3a
franck cuny <franck@lumberjaph.net>
2009-04-05 16:34:48 +0200

    db schema

---
+++ lib/intentioncloud/Model/DB.pm
@@ -0,0 +1,34 @@
+package intentioncloud::Model::DB;
+
+use strict;
+use base 'Catalyst::Model::DBIC::Schema';
+
+__PACKAGE__->config(
+    schema_class => 'intentioncloud::Schema',
+    connect_info => [
+        'dbi:SQLite:intentioncloud.db',
+        
+    ],
+);
+
+1;
+
+=head1 NAME
+
+intentioncloud::Model::DB - Catalyst DBIC Schema Model
+=head1 SYNOPSIS
+
+See L<intentioncloud>
+
+=head1 DESCRIPTION
+
+L<Catalyst::Model::DBIC::Schema> Model using schema L<intentioncloud::Schema>
+
+=head1 AUTHOR
+
+franck cuny,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.

b6533cabe03a4d075e7483509ea882d3b81a172f
franck cuny <franck@lumberjaph.net>
2009-04-05 16:34:16 +0200

    tidy, add forword to end, add last_clouds

--- lib/intentioncloud/Controller/Root.pm
+++ lib/intentioncloud/Controller/Root.pm
@@ -4,73 +4,38 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config->{ namespace } = '';
-
-=head1 NAME
-
-intentioncloud::Controller::Root - Root Controller for intentioncloud
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=cut
-
-=head2 index
-
-=cut
 
 sub about : Local {
     my ( $self, $c ) = @_;
-    $c->stash->{template} = 'others/what.tt';
+    $c->stash->{ template } = 'others/what.tt';
 }
 
 sub who : Local {
     my ( $self, $c ) = @_;
-    $c->stash->{template} = 'others/who.tt';
+    $c->stash->{ template } = 'others/who.tt';
 }
 
 sub why : Local {
     my ( $self, $c ) = @_;
-    $c->stash->{template} = 'others/why.tt';
+    $c->stash->{ template } = 'others/why.tt';
 }
 
 sub how : Local {
     my ( $self, $c ) = @_;
-    $c->stash->{template} = 'others/how.tt';
+    $c->stash->{ template } = 'others/how.tt';
 }
 
-sub index : Path : Args(0) {
+sub index : Local {
     my ( $self, $c ) = @_;
-    $c->stash->{template} = 'cloud/search.tt';
-
+    $c->stash->{ template } = 'cloud/search.tt';
+    $c->stash( last_clouds =>
+            [ $c->model( 'DB::Search' )->search()->slice( 0, 2 ) ] );
 }
-
-
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut 
 
 sub end : ActionClass('RenderView') {
+    my ( $self, $c ) = @_;
+    $c->forward('intentioncloud::View::TT');
 }
-
-=head1 AUTHOR
-
-franck cuny,,,
-
-=head1 LICENSE
-
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;

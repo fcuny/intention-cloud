fb2f9b68235c171195cd8dd07c5ac6a7df9eb774
franck cuny <franck@lumberjaph.net>
2009-04-05 15:00:55 +0200

    search.js

--- root/static/search.js
+++ root/static/search.js
@@ -1,14 +1,17 @@
 $(function() {
     $("#submit").click(function() {
         $('#spinner').show();
+        $('#view').hide();
         var q = $("input#q").val();
         var dataString = 'q='+ q;
         $.ajax({
             type: "POST",
             url: "search",
             data: dataString,
-            success: function() {
-                $('#view').html("<div id='message'>blah!</div>");
+            //dataType: "json",
+            success: function(response) {
+                alert(response);
+                $('#view').show().html(response);
                 $('#spinner').hide();
             }
         });
